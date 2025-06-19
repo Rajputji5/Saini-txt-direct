e264ad4949525fe7c9e780ed4b08e58397eb52f1
ASHISH RAJPUT <ashishsinghsikarwar61@gmail.com>
2025-06-19 17:55:52 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
+API_ID = int(environ.get("API_ID", "23480065"))
+API_HASH = environ.get("API_HASH", "32edb7d7fc1523b436109bff8ea061fc")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
+OWNER = int(environ.get("OWNER", "7760196814"))
+CREDIT = "🦁𝐓𝐇𝐀𝐊𝐔𝐑 𝐁𝐎𝐓𝐒🦁"
 LOG_CHANNEL = "-1002631537674" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
